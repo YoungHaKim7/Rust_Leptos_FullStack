48ebce951a809f0463bad3ba13ae158ee83f6100
GYoung <ytok1108@aol.com>
2024-09-09 00:38:37 +0900

    변수 Option으로 관리하기

---
+++ 001_Leptos_Tutorial/001_Part1_Building_User_Interfaces/3_8_Error_Handling/a02_error_boundary/src/app.rs
@@ -0,0 +1,44 @@
+use leptos::{
+    component, create_signal, event_target_value, view, CollectView, ErrorBoundary, IntoView,
+    SignalGet,
+};
+
+#[component]
+pub fn App() -> impl IntoView {
+    view! {
+        <h1>{NumericInput} </h1>
+    }
+}
+
+#[component]
+fn NumericInput() -> impl IntoView {
+    let (value, set_value) = create_signal(Ok(0));
+
+    let on_input = move |ev| set_value(event_target_value(&ev).parse::<i32>());
+
+    view! {
+        <h1>"Error Handling"</h1>
+        <label>
+            "Type a number (or something that's not a number!)"
+            <input type="number" on:input=on_input/>
+            <ErrorBoundary
+                // the fallback receives a signal containing current errors
+                fallback=|errors| view! {
+                    <div class="error">
+                        <p>"Not a number! Errors: "</p>
+                        // we can render a list of errors as strings, if we'd like
+                        <ul>
+                            {move || errors.get()
+                                .into_iter()
+                                .map(|(_, e)| view! { <li>{e.to_string()}</li>})
+                                .collect_view()
+                            }
+                        </ul>
+                    </div>
+                }
+            >
+                <p>"You entered " <strong>{value}</strong></p>
+            </ErrorBoundary>
+        </label>
+    }
+}
---
+++ 001_Leptos_Tutorial/001_Part1_Building_User_Interfaces/3_8_Error_Handling/a02_error_boundary/src/main.rs
@@ -0,0 +1,7 @@
+use app::App;
+
+mod app;
+
+fn main() {
+    leptos::mount_to_body(App)
+}
