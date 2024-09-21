422322e1ed05beccb1e032474cee1f31ef1910e0
Global Young <67513038+YoungHaKim7@users.noreply.github.com>
2024-09-21 09:54:55 +0900

    SurrealDB & HomePage/ & TeamPage/(/team)

--- 003_Leptos_example_small_project/02_FullStack_App_Leptos_Actix_SurrealDB/actix-app01/src/app.rs
+++ 003_Leptos_example_small_project/02_FullStack_App_Leptos_Actix_SurrealDB/actix-app01/src/app.rs
@@ -43,19 +43,6 @@ pub fn App() -> impl IntoView {
     }
 }
 
-/// Renders the home page of your application.
-// #[component]
-// fn HomePage() -> impl IntoView {
-//     // Creates a reactive value to update the button
-//     let (count, set_count) = create_signal(0);
-//     let on_click = move |_| set_count.update(|count| *count += 1);
-
-//     view! {
-//         <h1>"Welcome to Leptos!"</h1>
-//         <button class="bg-red-500 rounded text-white px-2 py-2" on:click=on_click>"Click Me: " {count}</button>
-//     }
-// }
-
 /// 404 - Not Found
 #[component]
 fn NotFound() -> impl IntoView {
