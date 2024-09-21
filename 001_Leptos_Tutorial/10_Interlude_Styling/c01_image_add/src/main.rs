1047996fd9aea0f917d647a0e203e66bf7793e1c
GYoung <ytok1108@aol.com>
2024-09-21 18:12:58 +0900

    Leptos imgs test

---
+++ 001_Leptos_Tutorial/10_Interlude_Styling/c01_image_add/src/main.rs
@@ -0,0 +1,38 @@
+use leptos::{component, view, IntoView};
+use leptos_image::Image;
+use stylance::import_style;
+
+import_style!(style, "main.module.scss");
+
+#[component]
+pub fn MyImage() -> impl IntoView {
+    view! {
+        <Image
+            src="/imgs/test.jpg"
+            blur=true
+            width=750
+            height=500
+            quality=85
+        />
+    }
+}
+
+#[component]
+fn App() -> impl IntoView {
+    // leptos_image::provide_image_context();
+    view! {
+       <p class=style::one>"test p scss"</p>
+           <div class="one-"> one BackGround color Blue</div>
+               <h1 id="two">"Hello"</h1>
+               <h2 class="three-">"World"</h2>
+               <h3 class="red-">"friends!" </h3>
+            <figure>
+                <img src="test.jpg" class="test-image-"/>
+                <p>test test teset good imgs</p>
+            </figure>
+    }
+}
+
+fn main() {
+    leptos::mount_to_body(App)
+}
